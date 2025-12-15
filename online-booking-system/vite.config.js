5a79515c3b2b022ac437b44a95cdfbfd72fba46e
yzj0101 <2116223560@qq.com>
2025-12-16 01:28:44 +0800

    push full project to yinzhanjun

--- online-booking-system/vite.config.js
+++ online-booking-system/vite.config.js
@@ -4,5 +4,19 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: './'
+  base: './',
+  server: {
+    proxy: {
+      '/api/membership': {
+        target: 'http://localhost:8081',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/membership/, '/membership')
+      },
+      '/api': {
+        target: 'http://localhost:8082',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
